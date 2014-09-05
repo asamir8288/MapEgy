74975af676139aadfa60cf9106aa52b7ec4293c6
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-09-05 16:14:47 +0300

    Navigate to top in Careers page

--- application/views/frontend/list_all_jobs.php
+++ application/views/frontend/list_all_jobs.php
@@ -1,8 +1,8 @@
 <div class="contact-person">
-    <img src="<?php echo static_url();?>layout/images/career-advisor.png" />
+    <img src="<?php echo static_url(); ?>layout/images/career-advisor.png" />
     <span class="adviosr-title">Dr. rer. nat. Matthias</span>
     <span class="adviosr-job-title">Data Analysis and Visualization</span>
-    
+
     <a href="" class="apply-now"></a>
 </div>
 
@@ -33,4 +33,42 @@
         <div class="download-job-offer">Read the full offer here: <a href="<?php echo site_url('job/download_file/' . $job['id']); ?>">Download this job offer</a></div>
     <?php } ?>
 <?php } ?>
+
+<script>
+    $(document).ready(function() {
+
+        // hide #back-top first
+        $("#back-top").hide();
+
+        // fade in #back-top
+        $(function() {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 100) {
+                    $('#back-top').fadeIn();
+                } else {
+                    $('#back-top').fadeOut();
+                }
+            });
+
+            // scroll body to 0px on click
+            $('#back-top a').click(function() {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+            });
+        });
+
+    });
+</script>
+
+<style type="text/css">
+
+    
+</style>
+
+
+<p id="back-top">
+    <a href="#top"><span></span>Back to Top</a>
+</p>
 
