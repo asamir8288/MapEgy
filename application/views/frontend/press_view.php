4cb2cfd8b69ae33cbdbd9026a9dd4902567be3b8
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-09-11 02:19:12 +0300

    commit

--- application/views/frontend/press_view.php
+++ application/views/frontend/press_view.php
@@ -1,17 +1,25 @@
-<div class="press_top_left">
-    <div class="section-title"><?php echo $press['media_contact']['title']; ?></div>
-    <div class="section-desc"><?php echo $press['media_contact']['description']; ?></div>
-</div>
-<div id="Company-Profile" class="press_top_left">
-    <div class="section-title"><?php echo $press['company_profile']['title']; ?></div>
-    <div class="section-desc"><?php echo $press['company_profile']['description']; ?></div>
-</div>
-<div id="Management-Biographies" class="press_top_left">
-    <div class="section-title"><?php echo $press['management_biographies']['title']; ?></div>
-    <div class="section-desc"><?php echo $press['management_biographies']['description']; ?></div>
+<div style="float: left;width: 635px; ">
+    <div class="press_top_left">
+        <div class="section-title"><?php echo $press['media_contact']['title']; ?></div>
+        <div class="section-desc"><?php echo $press['media_contact']['description']; ?></div>
+    </div>
+    <div id="Company-Profile" class="press_top_left">
+        <div class="section-title"><?php echo $press['company_profile']['title']; ?></div>
+        <div class="section-desc"><?php echo $press['company_profile']['description']; ?></div>
+    </div>
+    <div id="Management-Biographies" class="press_top_left">
+        <div class="section-title"><?php echo $press['management_biographies']['title']; ?></div>
+        <div class="section-desc"><?php echo $press['management_biographies']['description']; ?></div>
+    </div>
 </div>
 <div class="press_top_right">
+    <div class="contact-person">
+        <img src="<?php echo static_url(); ?>layout/images/career-advisor.png" />
+        <span class="adviosr-title">Dr. rer. nat. Matthias</span>
+        <span class="adviosr-job-title">Data Analysis and Visualization</span>
 
+        <a href="mailto:start@mapegy.com" class="apply-now">Apply Now</a>
+    </div>
 </div>
 
 <div id="Press-Clipping" style="height: 10px;clear: left;"></div>
