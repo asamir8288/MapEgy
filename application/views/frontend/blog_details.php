bc0ac66dcb95113fec5704119db92d7627403372
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-09-14 22:56:08 +0300

    facebook comments plugin

--- application/views/frontend/blog_details.php
+++ application/views/frontend/blog_details.php
@@ -3,6 +3,18 @@
 
     <div class="blog-details-title"><?php echo $blog_details['title']; ?></div>
     <div class="blog-details-desc"><?php echo $blog_details['description']; ?></div>
+    
+    
+    <div id="fb-root" style="margin-top: 50px;"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1458587444431028&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-comments" data-href="<?php echo base_url();?>news/blog/details/<?php echo $blog_details['id'];?>" data-numposts="5" data-colorscheme="light"></div>
 </div>
 
 <div class="blog-details-right">
@@ -15,3 +27,4 @@
 </div>
 
 <div style="clear: left;height: 100px;"></div>
+
