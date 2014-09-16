eb3d076863a97be3d2e7c911f3f95de09e618c70
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-09-16 03:21:06 +0300

    commit

--- application/views/frontend/list_blogs.php
+++ application/views/frontend/list_blogs.php
@@ -1,5 +1,5 @@
 <?php foreach ($active_news as $news) { ?>
-    <div class="news_blog_box">
+    <div class="news_blog_box" style="height: 360px;">
         <img src="<?php echo static_url() . 'files/news/' . $news['image']; ?>" style="width: 271px; height: 176px;" />
         <div class="news-title"><?php echo $news['title']; ?></div>
         <div><?php echo substring($news['description'], $news['id'], 'news/blog/details', 25); ?></div>
