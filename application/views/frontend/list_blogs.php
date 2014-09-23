15dba7a234e51853ad0c7ef652c481636dcfae03
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-09-24 01:03:43 +0300

    Blog link to blog details

--- application/views/frontend/list_blogs.php
+++ application/views/frontend/list_blogs.php
@@ -3,8 +3,10 @@
 <?php foreach ($active_news as $news) { ?>
     <div class="news_blog_box" style="height: 360px;">
         <div class="news_date"><?php echo date('d M Y', strtotime($news['news_date'])); ?></div>
-        <img src="<?php echo static_url() . 'files/news/' . $news['image']; ?>" style="width: 271px; height: 176px;" />
-        <div class="news-title"><?php echo $news['title']; ?></div>
+        <a href="<?php echo site_url('news/blog/details/' . $news['id']); ?>" class="news-title">
+            <img src="<?php echo static_url() . 'files/news/' . $news['image']; ?>" style="width: 271px; height: 176px;" />
+        </a>
+        <a href="<?php echo site_url('news/blog/details/' . $news['id']); ?>" class="news-title"><?php echo $news['title']; ?></a>
         <div><?php echo substring($news['description'], $news['id'], 'news/blog/details', 25); ?></div>
     </div>
 <?php } ?>
