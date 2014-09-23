8e70a0b4ebaa77d28a2d9a60c435951d22e29573
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-09-24 01:06:44 +0300

    List All blogs in admin style

--- application/views/backend/news/list_news.php
+++ application/views/backend/news/list_news.php
@@ -3,9 +3,9 @@
 <table cellpadding="0" cellspacing="0">
     <tr>
         <th style="width: 100px;"><?php echo lang('news_image'); ?></th>
-        <th style="width: 150px;"><?php echo lang('news_title'); ?></th>        
+        <th style="width: 450px;"><?php echo lang('news_title'); ?></th>        
         <th style="width: 100px;margin-left: 10px;"><?php echo lang('news_date'); ?></th>
-        <th style="width: 200px;"><?php echo lang('news_set_in_homepage'); ?></th>
+        <th style="width: 120px;"><?php echo lang('news_set_in_homepage'); ?></th>
         <th></th>
         <th></th>
     </tr>
@@ -20,13 +20,13 @@
                         <input type="hidden" name="order_flag[]" value="<?php echo $news['id'];?>" />
                         <img style="width: 100px;height: 65px;" src="<?php echo static_url() . 'files/news/' . $news['image']; ?>" />
                     </td> 
-                    <td style="width: 200px;">
+                    <td style="width: 450px;">
                         <?php echo $news['title']; ?>
                     </td>                       
-                    <td style="width: 200px;">
+                    <td style="width: 100px;">
                         <?php echo date('d M, Y', strtotime($news['news_date']));?>
                     </td>   
-                    <td style="width: 50px;">
+                    <td style="width: 120px;text-align: center;">
                         <?php if ($news['set_in_homepage']) { ?>
                         <a style="z-index: 5000;" href="<?php echo base_url() . 'admin/manage_news/set_in_homepage/' . $news['id'] . '/0'; ?>"><img title="Set in Homepage" src="<?php echo static_url(); ?>layout/images/active.png" /></a>
                         <?php } else { ?>
