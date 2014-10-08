e9e2796e6bb50d936fb3d26317f26c5d9f0f9dd5
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-10-08 21:52:06 +0200

    add new banner

--- application/controllers/service.php
+++ application/controllers/service.php
@@ -18,7 +18,7 @@ class Service extends CI_Controller{
     public function index() {
         $this->data['menu'] = array(0,1,0,0,0,0);
         $this->data['page_title'] = 'Services';
-        $this->data['inside_banner'] = static_url() . 'layout/images/inside-banner.png';
+        $this->data['inside_banner'] = static_url() . 'layout/images/banner3.jpg';
 
         $this->data['consulting'] = ConsultingExpertTable::getOne();
 
