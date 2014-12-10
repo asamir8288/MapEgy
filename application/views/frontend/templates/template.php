f8caf7f8a20f54da75fc7c90f981879e0b7e0a3b
Ahmed Samir <ahmed.samir@basharsoft.com>
2014-12-10 23:10:33 +0200

    commiting

--- application/views/frontend/templates/template.php
+++ application/views/frontend/templates/template.php
@@ -42,6 +42,24 @@
 
             });
         </script>
+
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-35158292-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </head>
 
     <body>
@@ -61,7 +79,7 @@
                     $lang_class = 'en-lang-header';
                 }
                 ?>
-                <a class="<?php echo $lang_class;?>" href="<?php echo base_url('switch-language'); ?>"></a>                
+                <a class="<?php echo $lang_class; ?>" href="<?php echo base_url('switch-language'); ?>"></a>                
             </div>
 
             <ul id="top-menu">
